abfb6527ff7d90fa40aed7a5f7d66059253ba1f6
Maylla Rabay <mayllarabay@gmail.com>
2024-08-26 09:12:24 -0300

    feat: create recipe difficulty enum and recipe model

---
+++ src/app/domain/enums/difficulty-enum.ts
@@ -0,0 +1,5 @@
+export enum DifficultyEnum {
+  Easy = "Easy",
+  Medium = "Medium",
+  Hard = "Hard"
+}
---
+++ src/app/domain/models/recipe-model.ts
@@ -0,0 +1,7 @@
+export interface RecipeModel {
+  name: string
+  difficulty: string
+  position: number
+  index: number
+  imageUrl: string
+}
